ab6935379015300bba8ad5ea134f024de6b7d97d
Roman Prilipskii <rprilipskii@cloudlinux.com>
2024-05-31 12:37:57 +0200

    Add the CL7 to CL8 upgrader

---
+++ centos2almaconverter/actions/cl_installation.py
@@ -0,0 +1,64 @@
+# Copyright 1999 - 2024. Plesk International GmbH. All rights reserved.
+import os
+import shutil
+
+from pleskdistup.common import action, rpm, util
+
+
+class CloudLinuxLeappInstallation(action.ActiveAction):
+
+    def __init__(self):
+        self.name = "installing leapp"
+        self.pkgs_to_install = [
+            "leapp",
+            "python2-leapp",
+            "leapp-data-cloudlinux",
+            "leapp-upgrade"
+        ]
+
+    def _prepare_action(self) -> action.ActionResult:
+        if not rpm.is_package_installed("elevate-release"):
+            util.logged_check_call(["/usr/bin/yum", "install", "-y", "https://repo.cloudlinux.com/elevate/elevate-release-latest-el7.noarch.rpm"])
+
+        util.logged_check_call(["/usr/bin/yum-config-manager", "--enable", "elevate"])
+
+        util.logged_check_call(["/usr/bin/yum", "install", "-y"] + self.pkgs_to_install)
+        # We want to prevent the leapp packages from being updated accidentally to
+        # the latest version (for example by using 'yum update -y'). Therefore, we
+        # should disable the 'elevate' repository. Additionally, this will prevent
+        # the pre-checker from detecting leapp as outdated and prevent re-evaluation
+        # on the next restart.
+        util.logged_check_call(["/usr/bin/yum-config-manager", "--disable", "elevate"])
+        return action.ActionResult()
+
+    def _post_action(self) -> action.ActionResult:
+        rpm.remove_packages(
+            rpm.filter_installed_packages(
+                self.pkgs_to_install + ["elevate-release", "leapp-upgrade-el7toel8"]
+            )
+        )
+
+        leapp_related_files = [
+            "/root/tmp_leapp_py3/leapp",
+        ]
+        for file in leapp_related_files:
+            if os.path.exists(file):
+                os.unlink(file)
+
+        leapp_related_directories = [
+            "/etc/leapp",
+            "/var/lib/leapp",
+            "/var/log/leapp",
+            "/usr/lib/python2.7/site-packages/leapp",
+        ]
+        for directory in leapp_related_directories:
+            if os.path.exists(directory):
+                shutil.rmtree(directory)
+
+        return action.ActionResult()
+
+    def _revert_action(self) -> action.ActionResult:
+        return action.ActionResult()
+
+    def estimate_prepare_time(self) -> int:
+        return 40
--- centos2almaconverter/main.py
+++ centos2almaconverter/main.py
@@ -7,7 +7,9 @@ import pleskdistup.main
 import pleskdistup.registry
 
 import centos2almaconverter.upgrader
+import centos2almaconverter.cl_upgrader
 
 if __name__ == "__main__":
     pleskdistup.registry.register_upgrader(centos2almaconverter.upgrader.Centos2AlmaConverterFactory())
+    pleskdistup.registry.register_upgrader(centos2almaconverter.cl_upgrader.Centos2CLConverterFactory())
     sys.exit(pleskdistup.main.main())
