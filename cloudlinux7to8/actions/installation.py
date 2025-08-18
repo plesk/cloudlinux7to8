18f9c74d13f319bf0a82bd7abdce140435f297f5
SandakovMM <mmsandakov@gmail.com>
2025-08-18 16:48:22 +0300

    Disable cloudlinux-elevate repositories when packages are installed

--- cloudlinux7to8/actions/installation.py
+++ cloudlinux7to8/actions/installation.py
@@ -23,15 +23,15 @@ class LeappInstallation(action.ActiveAction):
         if not rpm.is_package_installed("elevate-release"):
             util.logged_check_call(["/usr/bin/yum", "install", "-y", self.elevate_release_rpm_url])
 
-        util.logged_check_call(["/usr/bin/yum-config-manager", "--enable", "elevate"])
+        util.logged_check_call(["/usr/bin/yum-config-manager", "--enable", "cloudlinux-elevate"])
 
         util.logged_check_call(["/usr/bin/yum", "install", "-y"] + self.pkgs_to_install)
         # We want to prevent the leapp packages from being updated accidentally to
         # the latest version (for example by using 'yum update -y'). Therefore, we
-        # should disable the 'elevate' repository. Additionally, this will prevent
+        # should disable the 'cloudlinux-elevate' repository. Additionally, this will prevent
         # the pre-checker from detecting leapp as outdated and prevent re-evaluation
         # on the next restart.
-        util.logged_check_call(["/usr/bin/yum-config-manager", "--disable", "elevate"])
+        util.logged_check_call(["/usr/bin/yum-config-manager", "--disable", "cloudlinux-elevate"])
         return action.ActionResult()
 
     def remove_all(self, include_logs: bool = True) -> None:
